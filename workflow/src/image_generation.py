148eac2c6bfe2bc42b0d018aac3912254197e4b2
Chris Lemke <1@lemke.ai>
2023-02-21 15:52:58 +0000

    refactor: improve error handling (#2)
    * ci: pre-commit autoupdate (#1)
    
    updates:
    - [github.com/charliermarsh/ruff-pre-commit: v0.0.247 → v0.0.249](https://github.com/charliermarsh/ruff-pre-commit/compare/v0.0.247...v0.0.249)
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    * resolve: version bump
    
    * ci: config auto commits for pre-commit hooks
    
    * feat: add completed error handling
    
    * resolve: remove entry from gitignore
    
    * resolve: remove entry from gitignore
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- workflow/src/image_generation.py
+++ workflow/src/image_generation.py
@@ -44,8 +44,27 @@ def make_request(prompt: str, size: int) -> str:
     except openai.error.AuthenticationError:
         return "🚨 There seems to be something wrong! Please check your API key."
 
-    except openai.error.InvalidRequestError:
-        return "🚨 You shouldn't generate such stuff! Your prompt was declined by OpenAI's safety system."
+    except openai.error.InvalidRequestError as error:
+        if str(error).startswith(
+            "Your request was rejected as a result of our safety system"
+        ):
+            return "🚨 You shouldn't generate such stuff! Your prompt was declined by OpenAI's safety system."
+        return "🚨 Hmmm... Something is wrong with your request. Try again later."
+
+    except openai.error.ServiceUnavailableError:
+        return "🚨 Oh no! The server is overloaded or not ready yet."
+
+    except openai.error.APIError:
+        return "🚨 D'oh! The server had an error while processing your request."
+
+    except openai.error.APIConnectionError:
+        return "🚨 There is something fishy with your internet connection. Check your network settings."
+
+    except openai.error.RateLimitError:
+        return "🚨 You have reached the rate limit. Check your settings in your OpenAI dashboard."
+
+    except openai.error.Timeout:
+        return "🚨 The request timed out. Try again later."
 
 
 __response = make_request(get_query(), __size)
